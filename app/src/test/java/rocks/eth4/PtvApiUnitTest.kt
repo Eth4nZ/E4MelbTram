ab3499387e0bf24f3d5d9481a5525860890c5a0b
ethan <aikszhang@gmail.com>
2018-01-20 22:55:09 +1100

    Try add unit test for PtvApi

---
+++ app/src/test/java/rocks/eth4/PtvApiUnitTest.kt
@@ -0,0 +1,14 @@
+package rocks.eth4
+
+import org.junit.Test
+
+
+/**
+ * Created by eth4 on 20/1/18.
+ */
+
+class PtvApiUnitTest {
+    @Test
+    fun get_route_types_test(){
+
+    }
--- app/src/test/java/rocks/eth4/PtvSignatureTest.kt
+++ app/src/test/java/rocks/eth4/PtvSignatureTest.kt
@@ -9,7 +9,7 @@ import rocks.eth4.e4melbtram.utils.PtvSignatureUtil
  * Created by eth4 on 19/1/18.
  */
 
-class ExampleUnitTest {
+class PtvSignatureUnitTest {
     @Test
     fun signature_test() {
         assertEquals(PtvKeyStore.expectedUrl, PtvSignatureUtil.buildTTAPIURL("/v3/route_types") )
